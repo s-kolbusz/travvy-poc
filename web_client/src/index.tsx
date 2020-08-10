763d6d77c95afcdbf9fd0be3a0b49fac634bcf89
skolbusz <sebcia7@gmail.com>
2020-08-10 17:44:55 +0200

    Add parcel-env types

--- web_client/src/index.tsx
+++ web_client/src/index.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-// @ts-ignore
 if (module.hot) {
-  // @ts-ignore
   module.hot.accept();
 }
 
